75c08cdf51c1215deef9752f0b8ac9d26d459fba
rapayel <luisrafaellagarda@gmail.com>
2025-11-03 17:25:29 -0600

    actualizacion de la base de datos

--- tablas.sql
+++ tablas.sql
@@ -1,13 +1,20 @@
-drop database if exists panelSolar;
-create database panelSolar;
-use panelSolar;
+DROP DATABASE IF EXISTS panelSolar;
+CREATE DATABASE panelSolar CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE panelSolar;
 
-create table Usuarios(
-	id int primary key auto_increment,
-    nombreCompleto varchar(75) not null,
-    nombreUsuario varchar(25) not null,
-    direccion varchar(125) not null,
-    correo varchar(50) not null,
-    contraseña varchar(20) not null unique,
-    rool varchar(15) not null
-);
+ALTER DATABASE panelSolar CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE TABLE Usuarios (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombreCompleto VARCHAR(75) NOT NULL,
+    nombreUsuario VARCHAR(25) NOT NULL,
+    direccion VARCHAR(125) NOT NULL,
+    correo VARCHAR(50) NOT NULL,
+    contraseña VARCHAR(100) NOT NULL,
+    rool VARCHAR(15) NOT NULL,
+    imagenPerfil VARCHAR(255)
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE Usuarios CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+INSERT INTO Usuarios(nombreCompleto, nombreUsuario, direccion, correo, contraseña, rool, imagenPerfil)
+VALUES ("Luis Rafael Lagarda Encinas", "xrapayel", "nose", "luisrafaellagarda@gmail.com", "abcdef123456@", "cliente", NULL);
+SELECT * FROM Usuarios;
